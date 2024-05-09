5ce646627bd7c8b655e0e58d0cc1c7f8d1798c8b
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2024-05-09 11:57:47 -0300

    LocalStorage, controle de acesso com redirect e Logout.

--- js/script.js
+++ js/script.js
@@ -292,6 +292,7 @@ const validaLogin = (input1, input2, event)=>{
         
         if((usuario.email === listaUsuarios[x].email) && (usuario.senha === listaUsuarios[x].senha)){
 
+                localStorage.setItem("usuario-logado", JSON.stringify(listaUsuarios[x]));
             msgStatus.setAttribute("class", "sucesso");
             msgStatus.innerText = "Login realizado com sucesso!"
 
