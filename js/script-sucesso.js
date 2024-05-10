63830bd760510e5647b590eb2bf61b351e4804fb
anacarolcg0 <anacarolcg0@gmail.com>
2024-05-10 10:06:21 -0300

    Corrigindo conflitos

---
+++ js/script-sucesso.js
@@ -0,0 +1,23 @@
+
+
+//Recuperando o token do usuário
+let tokenUser = sessionStorage.getItem("token");
+
+if(tokenUser != ""){
+    //Recuperando o objeto do localStorage
+    const usuario = JSON.parse(localStorage.getItem("usuario-logado"));
+    //Mensagem de Boas Vindas
+    const msgWelcome = document.querySelector("#msg-welcome");
+    msgWelcome.innerText = usuario.email;
+
+    //LOGOUT
+    const logoutUser = document.querySelector("#logout-user");
+    logoutUser.addEventListener("click", ()=>{
+        sessionStorage.removeItem("token");
+        
+        window.location.href = "../index.html";
+    });
+}else{
+    window.location.href = "../index.html";
+}
+
--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //Comentário de linha
 /*
 Comentário de bloco
@@ -8,8 +7,8 @@ Comentário de bloco
 // console.warn("Aviso");
 // console.info("Informação");
 
-//Recuperando o botão submit com a função getElementById(id) e guardando ele em uma constante.
-const btn = document.getElementById("btnSubmit");
+// //Recuperando o botão submit com a função getElementById(id) e guardando ele em uma constante.
+// const btn = document.getElementById("btnSubmit");
 //Atrelar um evento ao botão, no caso, ao clicar no botão, ele vai executar a função.
 
 // btn.addEventListener("click", function(){
@@ -36,7 +35,6 @@ const btn = document.getElementById("btnSubmit");
 
 // var teste;
 
-
 // Declaração de variáveis (var, let, const):
 // // Usando var
 // var x = 10;
@@ -58,10 +56,8 @@ const btn = document.getElementById("btnSubmit");
 // console.log(num);
 
 // // String
-// let str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est corporis nihil sit obcaecati quae repellendus excepturi quam saepe rerum nemo maiores veniam minus accusantium eveniet sequi natus, possimus eos blanditiis nam recusandae fugit labore assumenda nostrum. Quam error perferendis, sit quia rem et facere minus eum facilis, architecto voluptatem corrupti. Quia iure placeat necessitatibus rerum eveniet voluptates nostrum eos impedit architecto aspernatur perferendis omnis modi, animi iusto mollitia, neque dicta dolores, distinctio reiciendis deleniti pariatur quisquam? Sunt in, pariatur ut quidem obcaecati assumenda, deleniti reiciendis dolorem cumque, facilis officia natus esse minus tempora sint? Animi error quis qui consequatur dolore accusantium. Expedita minus architecto dolore aperiam? Vel cum atque dolorem totam architecto iste consectetur, nemo officia nulla vitae quidem molestiae aliquam aspernatur zureta ut quisquam debitis quae maiores adipisci! Animi aspernatur dolores officia deserunt repellat a eum ex! Voluptatem, ab! Quis voluptatum, ad dicta animi nobis molestias excepturi voluptates aspernatur nihil nam et aliquid impedit dolore veniam non consectetur eos quam, quidem vel quo, ducimus qui autem! Alias, voluptatem? Quis numquam, totam repellat non itaque illum explicabo inventore ad ut temporibus. Quidem labore harum laboriosam quo corrupti, atque obcaecati dolorem, itaque hic possimus molestias optio inventore incidunt voluptatum tempore, provident rem maxime natus odio nostrum non. Ipsum, tempora amet fugiat numquam officia voluptate esse rem quam cupiditate odit voluptatibus dicta molestias nisi modi illo sint, autem non perspiciatis assumenda porro in nemo nam. Neque quos voluptatum voluptas delectus nobis amet molestias. Esse laudantium doloribus aperiam, magni ducimus provident consequatur, maiores sunt soluta nostrum, ea dolor molestiae optio sit iste hic. Amet, praesentium quaerat in repudiandae autem quis aut mollitia nemo exercitationem, sunt magnam placeat impedit voluptatibus neque ullam eveniet consequatur aliquid ipsum facilis non ex. Laudantium sit est omnis provident doloremque cum. Tenetur, amet earum autem dolorem corrupti consequuntur suscipit incidunt itaque alias, non quis perferendis unde laboriosam corporis blanditiis facilis velit eos dicta quisquam magnam rem? At accusantium laboriosam, laudantium placeat modi quas, repudiandae est aliquam quis blanditiis quae ex ipsum doloremque maxime aliquid facilis commodi, aut fugit dolorem dolorum numquam itaque. Sunt modi fugiat harum minima hic saepe necessitatibus deleniti ratione voluptatum ipsam, exercitationem totam repellendus, eius facilis illum. Quaerat voluptate eveniet libero ullam incidunt ut laudantium, nisi doloribus maiores ipsam sunt magni tempore, voluptatibus quisquam repudiandae cum aut corrupti natus. Consectetur obcaecati provident rem sequi ullam. Porro, omnis incidunt dolorum nihil nostrum, dolores a pariatur delectus vitae deleniti perspiciatis doloribus, similique ea voluptas alias blanditiis tenetur odio nam dicta? Pariatur tenetur dolore similique? Quo doloremque nisi placeat iusto rerum accusantium labore nulla sit quidem perferendis libero, eos unde incidunt autem earum, atque corporis. Recusandae, nemo! Nemo eligendi dolore natus, cum sequi maxime numquam doloribus deserunt facere soluta, totam ullam perspiciatis beatae provident distinctio commodi incidunt inventore odit ipsum perferendis atque, pariatur iusto? Similique ipsum ex, tempore dolores iure itaque est harum corrupti tempora modi eaque amet temporibus odio qui mollitia recusandae sunt optio repudiandae delectus maxime minus accusamus facere, nam aspernatur! Unde hic eius obcaecati consectetur culpa? Sint iste architecto aliquid vero, doloribus nemo quis minus, eum ullam temporibus soluta sed sit corrupti cupiditate debitis iure exercitationem? Error sed ea et modi obcaecati quos praesentium aliquam rem, laudantium aperiam mollitia aut itaque fuga, eos reiciendis fugiat nesciunt? Facilis quae, corporis consequuntur nobis deleniti iste tenetur quaerat beatae excepturi doloribus natus nostrum similique error porro neque quis exercitationem repellat aspernatur in blanditiis delectus animi sint temporibus cupiditate. Fugit fugiat quasi ducimus odit laborum, ab distinctio rerum corporis provident beatae facilis atque ea, placeat error, consequuntur vel qui repudiandae quis quae aliquid itaque ullam excepturi. Maxime cumque molestias exercitationem aperiam consequatur incidunt, vel mollitia suscipit saepe iste laudantium quasi! Officia quis vero odit sunt quia ratione? Error libero, rem quaerat explicabo reiciendis enim temporibus sunt ex accusantium quia deserunt est, recusandae, dignissimos autem quas alias eos deleniti voluptate possimus in dolore sequi. Labore quaerat perferendis nesciunt suscipit deleniti! Nisi eligendi qui iusto quidem fugit itaque assumenda autem ratione dolor quo fuga quam blanditiis quasi repudiandae illo distinctio aliquid earum nam vitae temporibus alias, cupiditate natus maiores atque. Necessitatibus veritatis expedita, provident modi officia commodi earum ex fuga qui doloremque sed soluta. Voluptates deserunt omnis adipisci laudantium odio magnam tempore expedita quaerat a sunt minima atque libero eum ex et doloremque odit animi, modi debitis nemo recusandae aspernatur. Cupiditate at veritatis doloremque dolore amet ullam, itaque est quo aperiam voluptas fugiat. Harum eligendi, repudiandae aperiam inventore repellendus placeat dolore delectus similique laudantium nobis eos molestiae laboriosam sapiente ipsam asperiores necessitatibus architecto omnis, quidem quos? Expedita, ex quod alias sed laudantium, quis praesentium fugit error ea perferendis quia nulla totam odit sit ducimus dolor? Provident vero iste ea pariatur, perspiciatis dolorum illum autem ex. Voluptatibus facere facilis exercitationem ut sit tempore, dicta, expedita alias molestiae minus consectetur eum nesciunt deleniti atque. Quo quibusdam at iste! Fugiat in optio, saepe voluptatem necessitatibus aspernatur officia porro cum nihil architecto autem aliquam laudantium voluptates iusto illo quos ipsum at harum dolores! Quae molestiae enim fuga placeat perferendis dicta iste quia assumenda numquam reiciendis necessitatibus laboriosam, quam vero? Voluptatem nemo hic dignissimos molestiae nostrum, saepe maiores iure cupiditate natus atque laudantium dolor recusandae consectetur porro eligendi laboriosam a soluta nam esse quod? Praesentium necessitatibus sit, quo vero cumque reiciendis, soluta dolor doloremque illum cum, hic vel repellat similique consectetur. Voluptatum laborum harum, nulla iusto ipsam expedita perspiciatis sint possimus totam aliquam dicta adipisci vitae beatae modi impedit aut. Deserunt, quas! Ullam, itaque cupiditate? Numquam, perspiciatis. Harum, repellendus assumenda a est veritatis soluta officia laudantium recusandae facere quae incidunt dicta eligendi distinctio ipsum, maiores nulla consectetur optio ducimus molestias! Autem eius vel doloremque, eligendi excepturi porro earum odio perspiciatis amet accusantium esse quia expedita distinctio soluta provident dolorum incidunt nisi consectetur reiciendis! Tenetur consequatur sapiente laudantium libero sunt deserunt nostrum praesentium totam, consequuntur esse aliquid quod minus labore ratione fugiat magnam. Itaque maiores atque natus, assumenda autem nisi. Dignissimos libero recusandae ex molestias ad id, soluta itaque repellat esse nisi aliquid dolores velit qui assumenda doloribus ut? ";
 // console.log(str.charAt(0));
 // console.log(str.indexOf("mundo"));
-
 
 // //console.log(str.slice(5, 7))
 // console.log(str.indexOf("zureta"))
@@ -73,8 +69,8 @@ let u = console.log(str.charAt(6));
 let mu = m + u; */
 
 // // Boolean
-let bool = true;
-console.log(bool);
+// let bool = true;
+// console.log(bool);
 
 // // Array
 // let arr1 = [1, 2, 3, 4, 5];
@@ -112,9 +108,6 @@ console.log(bool);
 // console.log(idade);
 // console.log(cidade);
 
-
-// // EXERCÍCIOS
-
 // // Conversão entre tipos de dados:
 
 // // Converter string para número
@@ -132,9 +125,6 @@ console.log(bool);
 // let booleano = Boolean(valor);
 // console.log(booleano); // Saída: false (0 é considerado false)
 
-
-// EXERCÍCIOS
-
 // Exercício 1 - Variáveis e Hoisting:
 // Qual será o valor de nome após a execução do código abaixo?
 // "use strict";
@@ -147,7 +137,7 @@ console.log(bool);
 // Exercício 2 - Declaração de Variáveis:
 // Declare uma variável estrito utilizando let sem atribuir um valor. Imprima o valor de estrito no console.
 // let estrito;
-// console.log(estrito); // Saída: undefined
+// console.log(estrito);
 
 // Exercício 3 - Tipos de Dados:
 // Qual é o tipo de dado da variável str?
@@ -159,7 +149,7 @@ console.log(bool);
 // Exercício 5 - Acesso a Propriedades de Objetos:
 // Acesse a propriedade idade do objeto obj e imprima seu valor no console.
 // let obj = { nome: "João", idade: 25, devedor: true };
-// console.log(obj.idade); // Saída: 25
+// console.log(obj.idade);
 
 // Exercício 6 - Atributo Variável de Objetos:
 // Atribua a string "São Paulo" à propriedade cidade do objeto obj. Imprima obj no console.
@@ -186,39 +176,102 @@ console.log(bool);
 // Exercício 10 - Mesclar Arrays com Spread:
 // Crie um novo array chamado arr4 que seja a concatenação dos arrays arr1 e arr2 utilizando o operador spread. Imprima arr4 no console.
 
-
 let listaUsuarios = [
-    {email:"email@email.com",senha: "123456"},
-    {email:"jose@email.com",senha: "123456"},
-    {email:"joao@email.com",senha: "123456"},
-    {email:"maria@email.com",senha: "123456"},
-
-];
-
-// Recuperar o botão de submit através da fynção querySelector.
-const btnSubmit = document.querySelector("button[type=submit]");
-console.log(btnSubmit.innerHTML);
-// Atrelando o evento click ao botão.
-btnSubmit.addEventListener("click",()=>{
-    // Pegando os valores dos inputs pelo método value.
+    { email: "email@email.com", senha: "123456" },
+    { email: "jose@email.com", senha: "123456" },
+    { email: "joao@email.com", senha: "123456" },
+    { email: "maria@email.com", senha: "123456" },
+  ];
+  
+  // //Recuperar o botão de submit através da função querySelector.
+  // const btnSubmit = document.querySelector("button[type=submit]");
+  // //Atrelando o evento click ao botão.
+  // btnSubmit.addEventListener("click",()=>{
+  
+  //     let emailUser = document.querySelector("input[type=email]").value;
+  //     let senhaUser = document.querySelector("input[type=password]").value;
+  
+  //     //Popular o objeto com os dados do usuário.
+  //     usuario.email = emailUser;
+  //     usuario.senha = senhaUser;
+  
+  // });
+  
+  //Apresentação da senha
+  function apresentaSenha(input2) {
+  
+      if(input2.type == "password"){
+          input2.setAttribute("type","text");
+      }else{
+          input2.setAttribute("type","password");
+      }
+  }
+  
+  let eyeIcon = document.querySelector("i");
+  
+  eyeIcon.addEventListener("click", function(){ 
+      //Carregando o input senha:
+      const inputSenha = document.querySelector("#idSenha");
+  
+      if(this.className == "fa-regular fa-eye"){
+          this.setAttribute("class","fa-regular fa-eye-slash");
+          apresentaSenha(inputSenha);
+      }else{
+          this.setAttribute("class","fa-regular fa-eye");
+          apresentaSenha(inputSenha);
+      }
+  });
+  
+  const validaLogin = (input1, input2, event) => {
+  
+      event.preventDefault();
+  
+    //Pegando os valores dos inputs pelo método value.
     const usuario = {
-        email:"",
-        senha:""
-    }
-
-    let emailUser = document.querySelector("input[type=email]").value;
-    let senhaUser = document.querySelector("input[type=password]").value;
-
-    //Popular o objeto com os dados do usuário.
-    usuario.email = emailUser;
-    usuario.senha = senhaUser;
-
+      email: input1.value,
+      senha: input2.value,
+    };
+  
+  
+  
+    //Recuperando elemento de mensagem.
+    const msgStatus = document.querySelector(".valida");
+  
     for (let x = 0; x < listaUsuarios.length; x++) {
-        
-        if((usuario.email === listaUsuarios[x].email) && (usuario.senha === listaUsuarios[x].senha)){
-            console.log("Usuário validado")
-        }
-        
+      if (
+        usuario.email === listaUsuarios[x].email &&
+        usuario.senha === listaUsuarios[x].senha
+      ) {
+  
+      localStorage.setItem("usuario-logado", JSON.stringify(listaUsuarios[x]));
+  
+      //Criar um String-token e colocar no sessionStoarge
+      const userToken = Math.random().toString(16).substring(2);
+      //Criando o token e colocando no sessionStorage.
+      sessionStorage.setItem("token",userToken);
+  
+        msgStatus.setAttribute("class", "sucesso");
+        msgStatus.innerText = "Login realizado com Sucesso!";
+  
+        setTimeout(() => {
+          msgStatus.setAttribute("class", "valida");
+          msgStatus.innerText = "";
+          window.location.href = "../status/sucesso.html";
+        }, 3000);
+  
+        return true;
+      }
     }
-
-})
+  
+    msgStatus.setAttribute("class", "erro");
+    msgStatus.innerText = "Nome de usuario ou senha incoretos!";
+  
+    setTimeout(() => {
+      msgStatus.setAttribute("class", "valida");
+      msgStatus.innerText = "";
+      window.location.href = "../status/erro.html";
+    }, 3000);
+  
+    return false;
+  };
+  
