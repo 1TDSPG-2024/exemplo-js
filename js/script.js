3caba016a3e29862ebda0ae7b23de98a2e7fbcfa
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-05-10 09:23:16 -0300

    apresentar senha criado.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //Comentário de linha
 /*
 Comentário de bloco
@@ -8,8 +7,8 @@ Comentário de bloco
 // console.warn("Aviso");
 // console.info("Informação");
 
-//Recuperando o botão submit com a função getElementById(id) e guardando ele em uma constante.
-const btn = document.getElementById("btnSubmit");
+// //Recuperando o botão submit com a função getElementById(id) e guardando ele em uma constante.
+// const btn = document.getElementById("btnSubmit");
 //Atrelar um evento ao botão, no caso, ao clicar no botão, ele vai executar a função.
 
 // btn.addEventListener("click", function(){
@@ -22,34 +21,33 @@ const btn = document.getElementById("btnSubmit");
 
 // "use strict"
 //Hoisting
-let nome = "Alexandre";
+// let nome = "Alexandre";
 
-if(true)
-{
-    let nome = "João";
-}
+// if(true)
+// {
+//     let nome = "João";
+// }
 
-console.log(nome);
+// console.log(nome);
 
-teste = "Olá";
-console.log(teste);
+// teste = "Olá";
+// console.log(teste);
 
-var teste;
-
+// var teste;
 
 // Declaração de variáveis (var, let, const):
-// Usando var
+// // Usando var
 // var x = 10;
 // console.log(x);
 
-// // Usando let
+// // // Usando let
 // let y = 20;
 // console.log(y);
 
-// // Usando const (constante, valor não pode ser alterado)
-const z = 30;
-console.log(z);
-// z = 20;
+// // // Usando const (constante, valor não pode ser alterado)
+// const z = 30;
+// console.log(z);
+// // z = 20;
 
 // Tipos de dados em JavaScript:
 
@@ -58,9 +56,17 @@ console.log(z);
 // console.log(num);
 
 // // String
-// let str = "Olá, mundo!";
 // console.log(str.charAt(0));
-// console.log(str.indexOf("Olá"));
+// console.log(str.indexOf("mundo"));
+
+// //console.log(str.slice(5, 7))
+// console.log(str.indexOf("zureta"))
+// console.log(str.slice(924, 930))
+/* let m = console.log(str.charAt(5));
+let u = console.log(str.charAt(6));
+let mu = m + u;
+let u = console.log(str.charAt(6));
+let mu = m + u; */
 
 // // Boolean
 // let bool = true;
@@ -102,9 +108,6 @@ console.log(z);
 // console.log(idade);
 // console.log(cidade);
 
-
-// // EXERCÍCIOS
-
 // // Conversão entre tipos de dados:
 
 // // Converter string para número
@@ -122,8 +125,6 @@ console.log(z);
 // let booleano = Boolean(valor);
 // console.log(booleano); // Saída: false (0 é considerado false)
 
-// EXERCÍCIOS
-
 // Exercício 1 - Variáveis e Hoisting:
 // Qual será o valor de nome após a execução do código abaixo?
 // "use strict";
@@ -136,35 +137,22 @@ console.log(z);
 // Exercício 2 - Declaração de Variáveis:
 // Declare uma variável estrito utilizando let sem atribuir um valor. Imprima o valor de estrito no console.
 // let estrito;
-// console.log(estrito); // Saída: undefined
+// console.log(estrito);
 
 // Exercício 3 - Tipos de Dados:
 // Qual é o tipo de dado da variável str?
 // let str = "Olá, mundo!";
 
-
 // Exercício 4 - Mesclar Arrays:
 // Crie um novo array chamado arrConcatenado que seja a concatenação dos arrays arr1 e arr2. Imprima arrConcatenado no console.
-let arr1 = [];
-let arr2 = [];
-let arrConcatenado = arr1.concat(arr2);
-console.log(arrConcatenado);
 
 // Exercício 5 - Acesso a Propriedades de Objetos:
 // Acesse a propriedade idade do objeto obj e imprima seu valor no console.
 // let obj = { nome: "João", idade: 25, devedor: true };
-// console.log(obj.idade); // Saída: 25
+// console.log(obj.idade);
 
 // Exercício 6 - Atributo Variável de Objetos:
 // Atribua a string "São Paulo" à propriedade cidade do objeto obj. Imprima obj no console.
-let obj = {cidade: "São Paulo"}
-console.log(obj);
-console.table(obj);
-console.log(obj.cidade);
-console.table(obj.cidade);
-let atributo = "cidade"
-console.log("Atrinuto variado : " + obj[atributo]);
-
 
 // Exercício 7 - Conversão de String para Número:
 // Converta a string "10" em um número e imprima o resultado no console.
@@ -188,36 +176,97 @@ console.log("Atrinuto variado : " + obj[atributo]);
 // Exercício 10 - Mesclar Arrays com Spread:
 // Crie um novo array chamado arr4 que seja a concatenação dos arrays arr1 e arr2 utilizando o operador spread. Imprima arr4 no console.
 
-let listaUsuarios = [{
-    email:"email@Gmail.com", senha:"123456",},
-    {email:"jose@Gmail.com", senha:"123435"},
-    {emial:"Joao@Gmail.com", senha:"123456"},
-    {email:"maria@Gmai.com", senha:"123456"}
-];
-
-//Recuperar o botão de submit através da função querySelector.
-const btnSubmit = document.querySelector("button[type=submit]");
-//Atrelando o evento click ao botão.
-btnSubmit.addEventListener("click",()=>{
+let listaUsuarios = [
+    { email: "email@email.com", senha: "123456" },
+    { email: "jose@email.com", senha: "123456" },
+    { email: "joao@email.com", senha: "123456" },
+    { email: "maria@email.com", senha: "123456" },
+  ];
+  
+  // //Recuperar o botão de submit através da função querySelector.
+  // const btnSubmit = document.querySelector("button[type=submit]");
+  // //Atrelando o evento click ao botão.
+  // btnSubmit.addEventListener("click",()=>{
+  
+  //     let emailUser = document.querySelector("input[type=email]").value;
+  //     let senhaUser = document.querySelector("input[type=password]").value;
+  
+  //     //Popular o objeto com os dados do usuário.
+  //     usuario.email = emailUser;
+  //     usuario.senha = senhaUser;
+  
+  // });
+  
+  //Apresentação da senha
+  function apresentaSenha(input2) {
+  
+      if(input2.type == "password"){
+          input2.setAttribute("type","text");
+      }else{
+          input2.setAttribute("type","password");
+      }
+  }
+  
+  let eyeIcon = document.querySelector("i");
+  
+  eyeIcon.addEventListener("click", function(){ 
+      //Carregando o input senha:
+      const inputSenha = document.querySelector("#idSenha");
+  
+      if(this.className == "fa-regular fa-eye"){
+          this.setAttribute("class","fa-regular fa-eye-slash");
+          apresentaSenha(inputSenha);
+      }else{
+          this.setAttribute("class","fa-regular fa-eye");
+          apresentaSenha(inputSenha);
+      }
+  });
+  
+  const validaLogin = (input1, input2, event) => {
+  
+      event.preventDefault();
+  
     //Pegando os valores dos inputs pelo método value.
-    const usuario ={
-        email: "", 
-        senha: ""
+    const usuario = {
+      email: input1.value,
+      senha: input2.value,
+    };
+  
+  
+  
+    //Recuperando elemento de mensagem.
+    const msgStatus = document.querySelector(".valida");
+  
+    for (let x = 0; x < listaUsuarios.length; x++) {
+      if (
+        usuario.email === listaUsuarios[x].email &&
+        usuario.senha === listaUsuarios[x].senha
+      ) {
+  
+      localStorage.setItem("usuario-logado", JSON.stringify(listaUsuarios[x]));
+  
+        msgStatus.setAttribute("class", "sucesso");
+        msgStatus.innerText = "Login realizado com Sucesso!";
+  
+        setTimeout(() => {
+          msgStatus.setAttribute("class", "valida");
+          msgStatus.innerText = "";
+          window.location.href = "../status/sucesso.html";
+        }, 3000);
+  
+        return true;
+      }
     }
-    let emailUser = document.querySelector("input[type=email]").Value;
-    let senhaUser = document.querySelector("input[type=password]").Value;
-    console.log(emailUser);
-    console.log(senhaUser);
-
-    //Popular o objeto com dados do usuário
-    usuario.email = emailUser;
-    usuario.senha = senhaUser;
-
-    for (let x = 0; x < listaUsuarios.length; x ++){
-        if((usuario.email === listaUsuarios[x].email &&
-            (usuario.senha === listaUsuario[x]).senha)){
-                console.log("usuário valido")
-            }
-    }
-
-})
+  
+    msgStatus.setAttribute("class", "erro");
+    msgStatus.innerText = "Nome de usuario ou senha incoretos!";
+  
+    setTimeout(() => {
+      msgStatus.setAttribute("class", "valida");
+      msgStatus.innerText = "";
+      window.location.href = "../status/erro.html";
+    }, 3000);
+  
+    return false;
+  };
+  
