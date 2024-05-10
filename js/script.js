1b098acb05b6b596e3c41b2b24746eb3f628f020
vitin46 <viegidulira@gmail.com>
2024-05-10 09:23:39 -0300

    Apresentar senha criado.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //Comentário de linha
 /*
 Comentário de bloco
@@ -8,8 +7,8 @@ Comentário de bloco
 // console.warn("Aviso");
 // console.info("Informação");
 
-//Recuperando o botão submit com a função getElementById(id) e guardando ele em uma constante.
-const btn = document.getElementById("btnSubmit");
+// //Recuperando o botão submit com a função getElementById(id) e guardando ele em uma constante.
+// const btn = document.getElementById("btnSubmit");
 //Atrelar um evento ao botão, no caso, ao clicar no botão, ele vai executar a função.
 
 // btn.addEventListener("click", function(){
@@ -36,7 +35,6 @@ const btn = document.getElementById("btnSubmit");
 
 // var teste;
 
-
 // Declaração de variáveis (var, let, const):
 // // Usando var
 // var x = 10;
@@ -61,7 +59,6 @@ const btn = document.getElementById("btnSubmit");
 // console.log(str.charAt(0));
 // console.log(str.indexOf("mundo"));
 
-
 // //console.log(str.slice(5, 7))
 // console.log(str.indexOf("zureta"))
 // console.log(str.slice(924, 930))
@@ -72,8 +69,8 @@ let u = console.log(str.charAt(6));
 let mu = m + u; */
 
 // // Boolean
-let bool = true;
-console.log(bool);
+// let bool = true;
+// console.log(bool);
 
 // // Array
 // let arr1 = [1, 2, 3, 4, 5];
@@ -128,7 +125,6 @@ console.log(bool);
 // let booleano = Boolean(valor);
 // console.log(booleano); // Saída: false (0 é considerado false)
 
-
 // Exercício 1 - Variáveis e Hoisting:
 // Qual será o valor de nome após a execução do código abaixo?
 // "use strict";
@@ -147,19 +143,16 @@ console.log(bool);
 // Qual é o tipo de dado da variável str?
 // let str = "Olá, mundo!";
 
-
 // Exercício 4 - Mesclar Arrays:
 // Crie um novo array chamado arrConcatenado que seja a concatenação dos arrays arr1 e arr2. Imprima arrConcatenado no console.
-
 
 // Exercício 5 - Acesso a Propriedades de Objetos:
 // Acesse a propriedade idade do objeto obj e imprima seu valor no console.
 // let obj = { nome: "João", idade: 25, devedor: true };
-// console.log(obj.idade); 
+// console.log(obj.idade);
 
 // Exercício 6 - Atributo Variável de Objetos:
 // Atribua a string "São Paulo" à propriedade cidade do objeto obj. Imprima obj no console.
-
 
 // Exercício 7 - Conversão de String para Número:
 // Converta a string "10" em um número e imprima o resultado no console.
@@ -184,65 +177,94 @@ console.log(bool);
 // Crie um novo array chamado arr4 que seja a concatenação dos arrays arr1 e arr2 utilizando o operador spread. Imprima arr4 no console.
 
 let listaUsuarios = [
-    {email:"email@email.com",senha:"123456"},
-    {email:"jose@email.com",senha:"123456"},
-    {email:"joao@email.com",senha:"123456"},
-    {email:"maria@email.com",senha:"123456"}
+  { email: "email@email.com", senha: "123456" },
+  { email: "jose@email.com", senha: "123456" },
+  { email: "joao@email.com", senha: "123456" },
+  { email: "maria@email.com", senha: "123456" },
 ];
 
-//Recuperar o botão de submit através da função querySelector.
-//const btnSubmit = document.querySelector("button[type=submit]");
-//Atrelando o evento click ao botão.
-//btnSubmit.addEventListener("click",()=>{
-//    let emailUser = document.querySelector("input[type=email]").value;
-//    let senhaUser = document.querySelector("input[type=password]").value;
-    
-    //Popular o objeto com os dados do usuário.
-//    usuario.email = emailUser;
-//    usuario.senha = senhaUser;
-    
-//});
+// //Recuperar o botão de submit através da função querySelector.
+// const btnSubmit = document.querySelector("button[type=submit]");
+// //Atrelando o evento click ao botão.
+// btnSubmit.addEventListener("click",()=>{
 
+//     let emailUser = document.querySelector("input[type=email]").value;
+//     let senhaUser = document.querySelector("input[type=password]").value;
 
-const validaLogin = (input1, input2)=>{
+//     //Popular o objeto com os dados do usuário.
+//     usuario.email = emailUser;
+//     usuario.senha = senhaUser;
+
+// });
+
+//apresentação da senha
+function apresentaSenha(input2) {
+
+    if(input2.type == "password"){
+      input2.setAttribute("type","text");
+    }else{
+      input2.setAttribute("type","password");
+    }
+};
+
+let eyeIcon = document.querySelector("i");
+
+eyeIcon.addEventListener("click", function(){
+  //CARREGANDO O INPUT senha:
+  const inputSenha = document.querySelector("#idSenha");
+
+  if(this.className == "fa-regular fa-eye"){
+    this.setAttribute("class", "fa-regular fa-eye-slash")
+    apresentaSenha(inputSenha);
+  }else{
+    this.setAttribute("class","fa-regular fa-eye");
+    apresentaSenha(inputSenha);
+  }
+});
+
+//apresentação da senha
+const validaLogin = (input1, input2, event) => {
 
     event.preventDefault();
 
-    //Pegando os valores dos inputs pelo método value.
-    const usuario = {
-        email: input1.value,
-        senha: input2.value
-    };
-    
-    //recuperando elemento de mensagem
-    const msgStatus = document.querySelector(".valida");
+  //Pegando os valores dos inputs pelo método value.
+  const usuario = {
+    email: input1.value,
+    senha: input2.value,
+  };
 
-    for (let x = 0; x < listaUsuarios.length; x++) {
-        
-        if((usuario.email === listaUsuarios[x].email) && (usuario.senha === listaUsuarios[x].senha)){
-            
-            msgStatus.setAttribute("class","sucesso");
-            msgStatus.innerText = "Login realizado com sucesso!";
+  //Recuperando elemento de mensagem.
+  const msgStatus = document.querySelector(".valida");
 
-            setTimeout(()=>{
-                msgStatus.setAttribute("class","sucesso");
-                msgStatus.innerText = "Login realizado com sucesso!";
-                window.location.href = "../status/sucesso.html";
-            }, 3000);
+  for (let x = 0; x < listaUsuarios.length; x++) {
+    if (
+      usuario.email === listaUsuarios[x].email &&
+      usuario.senha === listaUsuarios[x].senha
+    ) {
 
-            return false;
-        }
+    localStorage.setItem("usuario-logado", JSON.stringify(listaUsuarios[x]));
+
+      msgStatus.setAttribute("class", "sucesso");
+      msgStatus.innerText = "Login realizado com Sucesso!";
+
+      setTimeout(() => {
+        msgStatus.setAttribute("class", "valida");
+        msgStatus.innerText = "";
+        window.location.href = "../status/sucesso.html";
+      }, 3000);
+
+      return true;
     }
-    
-    msgStatus.setAttribute("class","Erro")
-    msgStatus.innerText = "Nome de usuário ou senha incorretos!";
-    
-    setTimeout(()=>{
-        msgStatus.setAttribute("class","Erro");
-        msgStatus.innerText = "Nome de usuário ou senha incorretos!";
-        window.href = "../status/erro.html";
-    }, 3000);
+  }
 
-    
-    return false;
-}
+  msgStatus.setAttribute("class", "erro");
+  msgStatus.innerText = "Nome de usuario ou senha incoretos!";
+
+  setTimeout(() => {
+    msgStatus.setAttribute("class", "valida");
+    msgStatus.innerText = "";
+    window.location.href = "../status/erro.html";
+  }, 3000);
+
+  return false;
+};
