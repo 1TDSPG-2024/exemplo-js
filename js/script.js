5bf4447e72834bbada11a8fda540e73d003ea078
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2024-05-17 09:46:37 -0300

    Alterações no modal

--- js/script.js
+++ js/script.js
@@ -324,9 +324,8 @@ const validaLogin = (input1, input2, event) => {
   return false;
 };
 
-const botao = document.querySelector("#btnOpen");
-const modal = document.querySelector("dialog");
-
-botao.addEventListener("click", function(){
-  modal.showModal();
-})
+const iniciaModal = ()=>{
+  const modal = document.querySelector("dialog");
+  modal.style.backgroundColor = "rgba(0,0,0, .8)"
+  modal.show();
+}
